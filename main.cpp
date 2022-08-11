090ae3d94411a9536af5833f5f2832a5e65d9981
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2022-08-11 17:40:26 +0200

    Reordering calling methods in main

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ int main()
     GPXData file1(nameOfFirstFile);
 
     cout << file1.getDistance() << endl;
-    cout << file1.getTime() << endl;
     cout << file1.getElevation() << endl;
+    cout << file1.getTime() << endl;
+
 
 
     return 0;
