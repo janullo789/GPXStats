78fe1d62bb8fb5f81db202cd0bd36d6485ca1fcc
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-02-13 23:24:22 +0100

    Init class GPXList

---
+++ GPXList.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Dell on 13.02.2023.
+//
+
+#include "GPXList.h"
+
+GPXList::GPXList(const char* path)
+{
+
+}
---
+++ GPXList.h
@@ -0,0 +1,18 @@
+//
+// Created by Dell on 13.02.2023.
+//
+
+#ifndef GPXSTATS_GPXLIST_H
+#define GPXSTATS_GPXLIST_H
+
+
+class GPXList
+{
+public:
+    GPXList(const char*);
+
+private:
+};
+
+
+#endif //GPXSTATS_GPXLIST_H
